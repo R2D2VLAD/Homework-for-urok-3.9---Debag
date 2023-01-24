1f61b76d231389a07f90703bf44c50099af7f7e0
Vlad <zitarvlad11@gmail.com>
2023-01-24 19:12:03 +0600

    Homework for urok 3.6 - Connecting libraries

--- src/main/java/com/example/homeworkforurok3_4webapplicationstructure/controllers/IngredientController.java
+++ src/main/java/com/example/homeworkforurok3_4webapplicationstructure/controllers/IngredientController.java
@@ -1,11 +1,16 @@
 package com.example.homeworkforurok3_4webapplicationstructure.controllers;
 import com.example.homeworkforurok3_4webapplicationstructure.model.Ingredient;
 import com.example.homeworkforurok3_4webapplicationstructure.services.impl.IngredientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/ingredient")
+@Tag(name = "Ингредиенты", description = "Endpoint-ы для упраления ингредиентами")
 public class IngredientController {
 
     private final IngredientService ingredientService;
@@ -14,12 +19,24 @@ public class IngredientController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Endpoint для добаления ингредиентов")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент успешно добавлен!")})
     public ResponseEntity<Long> addIngredient(@RequestBody Ingredient ingredient) {
         long id = ingredientService.addIngredient(ingredient);
         return ResponseEntity.ok(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Endpoint для редактирования ингредиентов по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент успешно изменен!")})
     public ResponseEntity<Ingredient> editIngredient(@PathVariable long id, @RequestBody Ingredient ingredient) {
         Ingredient ingredient1 = ingredientService.editIngredient(id, ingredient);
         if (ingredient1 == null) {
@@ -29,6 +46,12 @@ public class IngredientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Endpoint для удаления ингредиетов по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент успешно удалён!")})
     public ResponseEntity<Void> deleteIngredient(@PathVariable long id) {
         if (ingredientService.deleteIngredient(id)) {
             return ResponseEntity.ok().build();
@@ -37,6 +60,12 @@ public class IngredientController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Endpoint для получения ингредиентов по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент!")})
     public ResponseEntity<Ingredient> getIngredient(@PathVariable long id) {
         Ingredient ingredient = ingredientService.getIngredient(id);
         if (ingredient == null) {
@@ -46,6 +75,12 @@ public class IngredientController {
     }
 
     @GetMapping
+    @Operation(
+            summary = "Endpoint получения всех ингредиентов")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Список всех ингредиентов!")})
     public ResponseEntity<Void> getAllIngredient() {
         ingredientService.getAllIngredient();
         return ResponseEntity.ok().build();
--- src/main/java/com/example/homeworkforurok3_4webapplicationstructure/controllers/RecipeController.java
+++ src/main/java/com/example/homeworkforurok3_4webapplicationstructure/controllers/RecipeController.java
@@ -1,26 +1,40 @@
 package com.example.homeworkforurok3_4webapplicationstructure.controllers;
 import com.example.homeworkforurok3_4webapplicationstructure.model.Recipe;
 import com.example.homeworkforurok3_4webapplicationstructure.services.impl.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/recipe")
+@Tag(name = "Рецепты", description = "Endpoint-ы для упраления рецептами")
 public class RecipeController {
-
     private final RecipeService recipeService;
-
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
 
     @PostMapping
+    @Operation(
+           summary = "Endpoint для добаления рецептов")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт успешно добавлен!")})
     public ResponseEntity<Long> addRecipe(@RequestBody Recipe recipe) {
         long id = recipeService.addRecipe(recipe);
         return ResponseEntity.ok(id);
     }
-
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Endpoint для редактирования рецептов по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт успешно редактирован!")})
     public ResponseEntity<Recipe> editRecipe(@PathVariable long id, @RequestBody Recipe recipe) {
         Recipe recipe1 = recipeService.editRecipe(id, recipe);
         if (recipe1 == null) {
@@ -28,16 +42,26 @@ public class RecipeController {
         }
         return ResponseEntity.ok(recipe);
     }
-
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Endpoint для удаления рецептов по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт успешно удалён!")})
     public ResponseEntity<Void> deleteRecipe(@PathVariable long id) {
         if (recipeService.deleteRecipe(id)) {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.notFound().build();
     }
-
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Endpoint для получения информации о рецепте по id")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт")})
     public ResponseEntity<Recipe> getRecipe(@PathVariable long id) {
         Recipe recipe = recipeService.getRecipe(id);
         if (recipe == null) {
@@ -45,8 +69,13 @@ public class RecipeController {
         }
         return ResponseEntity.ok(recipe);
     }
-
     @GetMapping
+    @Operation(
+            summary = "Endpoint для получения информации о всех рецептов")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Список рецептов представлен")})
     public ResponseEntity<Void> getAllRecipe() {
         recipeService.getAllRecipe();
         return ResponseEntity.ok().build();
